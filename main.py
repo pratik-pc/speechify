d59778ec025c3859b986c020597e8ea312b15229
Pratik <pratikchanda2000@gmail.com>
2023-09-19 02:24:17 +0530

    feat: initialize pyqt gui app

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('project initialized')
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui.mainwindow import MainWindow
+
+def main():
+  app = QApplication(sys.argv)
+
+  main_window = MainWindow()
+
+  main_window.show()
+
+  sys.exit(app.exec_())
+
+if __name__ == "__main__":
+  main()
