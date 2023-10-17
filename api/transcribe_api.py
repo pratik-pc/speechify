c8cf5df96d3c45db554fccd2d3f64e66d05853c1
Pratik <pratikchanda2000@gmail.com>
2023-10-17 16:44:06 +0530

    feat: add api call to backend api post request to start transcription job on stop recording

---
+++ api/transcribe_api.py
@@ -0,0 +1,18 @@
+import requests
+
+def transcribe():
+  url = 'http://localhost:5000/api/transcribe'
+  audio_path = 'C:\project\speechify\sample.wav'
+  language = 'hi-IN'
+
+  data = {
+      'language': language,
+  }
+  try:
+    with open(audio_path, 'rb') as audio_file:
+        data['audio'] = (audio_file.name, audio_file, 'audio/wav')
+        print("Audio file opened successfully.")
+        response = requests.post(url, files=data)
+        print(response.text)
+  except IOError as e:
+      print(f"Failed to open the audio file: {str(e)}")
