d7deb7f92fb3c4d67395fe5228ed87d5be0faba7
Pratik <pratikchanda2000@gmail.com>
2023-11-08 01:49:43 +0530

    feat: change language code attribute with dropdown

--- gui/dropdown.py
+++ gui/dropdown.py
@@ -1,10 +1,21 @@
 from PyQt5.QtWidgets import QComboBox
 
+language_codes = {
+  'Hindi': 'hi-IN',
+  'Spanish': 'es-ES',
+  'French': 'fr-FR'
+}
+
 class Dropdown(QComboBox):
   def __init__(self, audiorec, parent=None):
     super().__init__(parent)
     self.audiorec = audiorec
     self.addItem("Hindi")
-    self.addItem("Bengali")
+    self.addItem("French")
     self.addItem("Spanish")
+    self.activated.connect(self.on_activated)
+
+
+  def on_activated(self):
+    self.audiorec.language_code = language_codes[self.currentText()]
     
