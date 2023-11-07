62f243fda460431145097b00de7473609158d02b
Pratik <pratikchanda2000@gmail.com>
2023-11-08 00:14:07 +0530

    feat: accept language code in transcribe_api

--- api/transcribe_api.py
+++ api/transcribe_api.py
@@ -1,18 +1,21 @@
 import requests
 
-def transcribe():
+def transcribe(language):
   url = 'http://localhost:5000/api/transcribe'
   audio_path = 'C:\project\speechify\sample.wav'
-  language = 'hi-IN'
+  # language = 'hi-IN'
 
   data = {
       'language': language,
   }
+  files = {
+     'audio': None
+  }
   try:
     with open(audio_path, 'rb') as audio_file:
-        data['audio'] = (audio_file.name, audio_file, 'audio/wav')
+        files['audio'] = (audio_file.name, audio_file, 'audio/wav')
         print("Audio file opened successfully.")
-        response = requests.post(url, files=data)
+        response = requests.post(url, files=files, data=data)
         return response.text
   except IOError as e:
       print(f"Failed to open the audio file: {str(e)}")
