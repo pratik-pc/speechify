bc79c2ecea4ebe20c7b2d400e14c354c03762c3b
Pratik <pratikchanda2000@gmail.com>
2023-11-17 14:17:27 +0530

    fix: fixes bug change_hotkey not working twice fixes the bug where keyboard ran on thread and invoking it multiple times failed. listener instance now runs locally on change_hotkey

--- gui/record_button.py
+++ gui/record_button.py
@@ -14,7 +14,6 @@ class PushButton(QPushButton):
   def __init__(self, hotkey_instance, parent=None):
     super().__init__('Record Keybind', parent)
     self.hotkey_instance = hotkey_instance        # Assign Global Hotkey instance 
-    self.hotkey_listener = keyboard.Listener(on_press=self.on_key_press)      # Create local keyboard listener to catch key for changing hotkey
     self.clicked.connect(self.change_hotkey)
 
   def on_key_press(self, key):
@@ -23,4 +22,5 @@ class PushButton(QPushButton):
       self.hotkey_listener.stop()         # Stop the local listener after changing the hotkey
 
   def change_hotkey(self):
+    self.hotkey_listener = keyboard.Listener(on_press=self.on_key_press)      # Create local keyboard listener to catch key for changing hotkey
     self.hotkey_listener.start()
